f0571b11db572f3afae2755b0818c23a3b5f759f
tia-cin <cintiaayelenarce@gmail.com>
2022-01-25 14:34:17 -0300

    done

--- clases/homeworks/ejercicio 1.ts
+++ clases/homeworks/ejercicio 1.ts
@@ -1,3 +1,5 @@
+// const-let-var : tipado = asignacion
+
 //Typescript va a inferir nuestra variable en la primer asignacion
 let variableInferida = 2;
 
@@ -10,15 +12,15 @@ let resultado = variableExplicita + variableInferida
 
 //completar el tipado correspondiente para cada variable
 
-let unString: null = 'mati';
+let unString: string = 'mati';
 
-let unNumber: null = 2;
+let unNumber: number = 2;
 
-let unBoolean: null = true;
+let unBoolean: boolean = true;
 
-let unNull: undefined = null; //que pasa aca?
+let unNull: null = null; //que pasa aca?
 
-let unUndefined: null = undefined //y aca?
+let unUndefined: undefined = undefined //y aca?
 
 	
 // Enum
@@ -27,6 +29,13 @@ enum Fases {
     Segunda, //1
     Tercera //2
 }
+
+// (function (Fases) {
+//     Fases[Fases['Primera'] = 0] = 'Primera';
+//     Fases[Fases['Segunda'] = 0] = 'Segunda';
+//     Fases[Fases['Tercera'] = 0] = 'Tercera';
+// })(Fases || (Fases = {}))
+
 let phase: Fases = Fases.Primera; // 0
 
 enum userActions {
@@ -34,5 +43,9 @@ enum userActions {
     postUser = "POST_USER" // "POST_USER"
 }
 
+// (function (userActions) {
+//     userActions['fetchUser'] = 'FETCH_USER';
+//     userActions['postUser'] = 'POST_USER';
+// })(userActions || (userActions = {}))
 
 export {}
